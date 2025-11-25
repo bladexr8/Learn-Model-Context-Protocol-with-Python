0cf562d1293cc88270b8626d2c3786335845d0fd
Stephen Moss <bladexr8@gmail.com>
2025-11-25 20:13:00 +1100

    Chapter02 basic client and stdio server

---
+++ my-code/Chapter02/server.py
@@ -0,0 +1,32 @@
+# STDIO Server
+import sys
+import json
+
+def send_response(response):
+    print(json.dumps(response))
+    sys.stdout.flush()
+
+while True:
+    for line in sys.stdin:
+        message = line.strip()
+        if message == "hello":
+            send_response("hello there")
+        elif message.startswith('{"jsonrpc":'):
+            json_message = json.loads(message)
+            match json_message['method']:
+                case "tools/list":
+                    response = {
+                        "jsonrpc": "2.0",
+                        "id": json_message["id"],
+                        "result": ["tool1","tool2"]
+                    }
+                    send_response(response)
+                    break
+                case _:
+                    send_response(f"Unknown method: {json_message['method']}")
+                    break
+        elif message == "exit":
+            send_response("Exiting server...")
+            sys.exit(0)
+        else:
+            send_response(f"Unknown message: {message}")
