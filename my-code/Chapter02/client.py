885d5369878da7f8ab781f90c7686362ed6fb8c4
Stephen Moss <bladexr8@gmail.com>
2025-11-25 21:08:25 +1100

    implemented initialization

--- my-code/Chapter02/client.py
+++ my-code/Chapter02/client.py
@@ -1,6 +1,8 @@
 import subprocess
 import json
 
+from utils.messages import list_tools_message, initialize_message, initialized_message
+
 # start the child process
 proc = subprocess.Popen(
     ['python3', 'server.py'],
@@ -18,6 +20,7 @@ list_tools_message = {
 
 message = 'hello\n'
 
+
 def send_message(message):
     """Send a message to the child process"""
     print(f'[CLIENT] Sending message to server... Message: {message.strip()}')
@@ -28,22 +31,44 @@ def serialize_message(message):
     """Serialize a message to JSON format"""
     return json.dumps(message) + '\n'
 
-# Send a message to the child
-send_message(message)
+def print_response(response, prefix = ""):
+    """Print the response from the server."""
+    try:
+        parsed = json.loads(response)
+        print(prefix,json.dumps(parsed, indent=2))
+    except json.JSONDecodeError:
+        print(prefix, response.strip())
 
-# Read response from child
-response = proc.stdout.readline()
-print('[SERVER]: ', response.strip())
+def connect():
+    print("Connecting to MCP Server...")
+    
+    # 1. Ask for capabilities
+    send_message(serialize_message(initialize_message))
+    # Read response from child/server
+    response = proc.stdout.readline()
+    print_response(response, prefix='[SERVER]: \n')
 
-# send a JSON-RPC message
-send_message(serialize_message(list_tools_message))
+    # 2. Send initialized notification, handshake is done
+    send_message(serialize_message(initialized_message))
 
-response = proc.stdout.readline()
-print('[SERVER]: ', response.strip())
+def list_tools():
+    # 3. send a message to list tools
+    # send a JSON-RPC message
+    send_message(serialize_message(list_tools_message))
+    response = proc.stdout.readline()
+    print_response(response, prefix='[SERVER]: \n')
 
-# this closes down the child aka server
-send_message('exit\n')
+def close_server():
+    # this closes down the child aka server
+    send_message('exit\n')
 
-proc.stdin.close()
-exit_code = proc.wait()
-print(f"Child exited with code {exit_code}")
+    proc.stdin.close()
+    exit_code = proc.wait()
+    print(f"Child exited with code {exit_code}")
+
+def main():
+    connect()
+    list_tools()
+    close_server()
+
+main()
--- my-code/Chapter02/server.py
+++ my-code/Chapter02/server.py
@@ -2,6 +2,10 @@
 import sys
 import json
 
+from utils.messages import initializeResponse
+
+initialized = False
+
 def send_response(response):
     print(json.dumps(response))
     sys.stdout.flush()
@@ -13,7 +17,27 @@ while True:
             send_response("hello there")
         elif message.startswith('{"jsonrpc":'):
             json_message = json.loads(message)
-            match json_message['method']:
+            method = json_message.get('method', '')
+
+            # connection not initialized
+            if not initialized:
+                if method != "initialize" and method != "notifications/initialized":
+                    print(f"Server not initialized. Please send an 'initialized' notification first. You sent {method}")
+                    sys.stdout.flush()
+                    continue
+
+            match method:
+                case "notifications/initialized":
+                    # print("Server initialized successfully")
+                    sys.stdout.flush()
+                    initialized = True
+                    break
+                case "initialize":
+                    print(json.dumps(initializeResponse))
+                    sys.stdout.flush()
+                    # initialized = True
+                    break
+                    # should return capabilities
                 case "tools/list":
                     response = {
                         "jsonrpc": "2.0",
